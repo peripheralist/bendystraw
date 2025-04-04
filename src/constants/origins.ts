2231913d5249bd0469c5e7e5888da562b97ce61b
peripheralist <peripheralist@gmail.com>
2025-04-04 14:11:24 -0600

    -

--- src/constants/origins.ts
+++ src/constants/origins.ts
@@ -1,5 +1,6 @@
 export const ALLOWED_ORIGINS = [
   "https://retail.banny.eth.sucks",
+  "https://banny-retail-bendystraw.vercel.app/",
   "https://juicebox.money",
   "https://*.juicebox.money",
 ];
