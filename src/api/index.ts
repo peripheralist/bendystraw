2c2bc45a40443ef177ac9ecf2cb1a13b9167aa99
peripheralist <peripheralist@gmail.com>
2025-04-15 17:07:19 -0600

    -

--- src/api/index.ts
+++ src/api/index.ts
@@ -5,25 +5,28 @@ import { db } from "ponder:api";
 import schema from "ponder:schema";
 import { ALLOWED_ORIGINS } from "../constants/origins";
 import { keyAuthMiddleware } from "../middleware/keyAuth";
-import { rateLimitMiddleware } from "../middleware/rateLimit";
 
 const app = new Hono();
 
 // public testing
-if (process.env.NODE_ENV !== "development") app.use("/", rateLimitMiddleware);
+// if (process.env.NODE_ENV !== "development") app.use("/", rateLimitMiddleware); // disable rate limiting until CORS works
 app.use("/", graphql({ db, schema }));
 
 // origin restricted (internal apps)
 app.use("/graphql", async (c, next) => {
   // only allow requests, not yoga UI (GET)
   if (c.req.method !== "POST") return c.text("Not allowed", 401);
-
   await next();
 });
 app.use("/graphql", cors({ origin: ALLOWED_ORIGINS }));
 app.use("/graphql", graphql({ db, schema }));
 
 // require key (internal dev, external apps)
+app.use("/graphql/:key", async (c, next) => {
+  // only allow requests, not yoga UI (GET)
+  if (c.req.method !== "POST") return c.text("Not allowed", 401);
+  await next();
+});
 app.use("/graphql/:key", keyAuthMiddleware);
 app.use("/graphql/:key", graphql({ db, schema }));
 
