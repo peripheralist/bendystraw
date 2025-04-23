d13cbbf32fe9161fd3f8ee6b23f12e65aeccdc0e
peripheralist <peripheralist@gmail.com>
2025-04-23 15:54:38 -0600

    cors working 2?

--- src/constants/origins.ts
+++ src/constants/origins.ts
@@ -1,6 +1,8 @@
 export const ALLOWED_ORIGINS = [
   "https://retail.banny.eth.sucks",
-  "https://banny-retail-bendystraw.vercel.app/",
+  "https://retail.banny.eth.sucks/*",
   "https://juicebox.money",
+  "https://juicebox.money/*",
   "https://*.juicebox.money",
+  "https://*.juicebox.money/*",
 ];
