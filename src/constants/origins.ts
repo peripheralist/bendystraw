6889c993f11665d79596dd045486330dc18becd6
peripheralist <peripheralist@gmail.com>
2025-05-08 15:18:40 -0600

    no wildcard origins

--- src/constants/origins.ts
+++ src/constants/origins.ts
@@ -1,14 +1,8 @@
 export const ALLOWED_ORIGINS = [
   "https://retail.banny.eth.sucks",
-  "https://retail.banny.eth.sucks/*",
   "https://juicebox.money",
-  "https://juicebox.money/*",
-  "https://*.juicebox.money",
-  "https://*.juicebox.money/*",
+  "https://www.juicebox.money",
   "https://app.revnet.eth.sucks",
-  "https://app.revnet.eth.sucks/*",
   "https://revnet.app",
-  "https://revnet.app/*",
-  "https://*.revnet.app",
-  "https://*.revnet.app/*",
+  "https://www.revnet.app",
 ];
