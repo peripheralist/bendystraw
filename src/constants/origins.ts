720deec56460a704cf8b222881d10e448eb11d5d
peripheralist <peripheralist@gmail.com>
2025-05-30 14:35:02 -0600

    update origins

--- src/constants/origins.ts
+++ src/constants/origins.ts
@@ -1,6 +1,7 @@
 export const ALLOWED_ORIGINS = [
   "https://retail.banny.eth.sucks",
   "https://juicebox.money",
+  "https://sepolia.juicebox.money",
   "https://www.juicebox.money",
   "https://app.revnet.eth.sucks",
   "https://revnet.app",
