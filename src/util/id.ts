d5d1f1863ce14b3226b838bb96a322ef6fca0a7a
peripheralist <peripheralist@gmail.com>
2025-09-17 20:44:04 -0600

    deterministic ids

--- src/util/id.ts
+++ src/util/id.ts
@@ -1,1 +1,10 @@
-export const generateId = () => crypto.randomUUID();
+import { Version } from "./getVersion";
+
+export const idForProject = (
+  version: Version,
+  projectId: number | bigint,
+  chainId: number
+) => `${version}_${projectId}_${chainId}`;
+
+export const idForSuckerGroup = (projects: string[]) =>
+  projects.sort().join("-");
