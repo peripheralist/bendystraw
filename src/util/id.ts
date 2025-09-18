62a5d15506c339f98cdc31b9b09e4ec094466bc9
peripheralist <peripheralist@gmail.com>
2025-09-17 21:26:08 -0600

    keccack hash suckerGroupIds. project ids use '-'

--- src/util/id.ts
+++ src/util/id.ts
@@ -1,10 +1,18 @@
+import { keccak256 } from "viem";
 import { Version } from "./getVersion";
 
 export const idForProject = (
   version: Version,
   projectId: number | bigint,
   chainId: number
-) => `${version}_${projectId}_${chainId}`;
+) => `${version}-${projectId}-${chainId}`;
 
-export const idForSuckerGroup = (projects: string[]) =>
-  projects.sort().join("-");
+export const idForSuckerGroup = (projects: string[]) => {
+  const str = projects.sort().join("-");
+
+  const arr = new TextEncoder().encode(str);
+
+  const hash = keccak256(arr);
+
+  return hash.substring(2, 34); // trim leading '0x' and use next 32 characters
+};
